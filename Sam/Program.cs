73e663956efe59eca49075087cd21ce32760e756
jeffvarghese5 <47558882+Amberside@users.noreply.github.com>
2023-08-28 12:58:02 +1000

    Some files added
    Some files added

---
+++ Sam/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myNum = 15;
+            myNum = 20;
+            Console.WriteLine(myNum);
+            //Sam
+        }
+    }
+}
